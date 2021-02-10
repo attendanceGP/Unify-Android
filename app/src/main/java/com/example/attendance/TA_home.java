f88b1150af80cd0d45c15965037421e9c4e5319a
youssef mohamed mohamed shehata <y0y01311999@gmail.com>
2021-02-10 19:21:56 +0200

    linked post attendance api with android

--- app/src/main/java/com/example/attendance/TA_home.java
+++ app/src/main/java/com/example/attendance/TA_home.java
@@ -1,11 +1,26 @@
 package com.example.attendance;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TA_home extends AppCompatActivity {
     TextView TA_name;
@@ -24,8 +39,34 @@ public class TA_home extends AppCompatActivity {
 
         TA_name = findViewById(R.id.TAname);
         TA_id = findViewById(R.id.TAid);
+        recordAttendance = findViewById(R.id.RecordAttendance);
 
         TA_name.setText(sessionManager.getName());
         TA_id.setText(sessionManager.getId().toString());
+
+        recordAttendance.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                UserAPI userAPI = APIClient.getClient().create(UserAPI.class);
+
+                java.util.Date date=new java.util.Date();
+
+                Call<String> call = userAPI.taStartAttendance(new SimpleDateFormat("dd-MM-yyyy").format(date),"CS_DS_1","CS467",sessionManager.getId());
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if (response.code() != 200) {
+                            Toast.makeText(getApplicationContext(), "an error occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "please check your internet connection", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+        });
+}
 }
--- app/src/main/java/com/example/attendance/UserAPI.java
+++ app/src/main/java/com/example/attendance/UserAPI.java
@@ -1,8 +1,11 @@
 package com.example.attendance;
 
+import java.util.Date;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface UserAPI {
@@ -11,4 +14,8 @@ public interface UserAPI {
 
     @GET("user/gettoken")
     Call<String> getToken(@Query("id") Integer id);
+
+    @POST("ta/postattendance")
+    Call<String> taStartAttendance(@Query("date") String date,
+                          @Query("userGroup") String userGroup, @Query("courseId") String courseId, @Query("userId") Integer userId);
 }

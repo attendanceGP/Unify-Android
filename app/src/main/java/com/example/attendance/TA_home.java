7a516e630f4332aaf690ad721832d178b85baa76
youssef mohamed mohamed shehata <y0y01311999@gmail.com>
2021-02-11 19:17:49 +0200

    added course list , record attendance and groups
    added an api to help get the list of courses that the  TA teaches and made a dropdown list using them to give the TA a choice of what course he will start attendance on, allowed the TA to write as many groups as he wants in an edit text box, so that TAs can give attendance to multiple groups for the same course at once without having to record attendance twice, group names must be separated by spaces

--- app/src/main/java/com/example/attendance/TA_home.java
+++ app/src/main/java/com/example/attendance/TA_home.java
@@ -1,34 +1,36 @@
 package com.example.attendance;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class TA_home extends AppCompatActivity {
+public class TA_home extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView TA_name;
     TextView TA_id;
     SessionManager sessionManager;
     EditText groups;
     Button recordAttendance;
-    Button selectCourse;
+    Spinner selectCourse;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> givenCourses = new ArrayList<String>();
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +38,45 @@ public class TA_home extends AppCompatActivity {
         setContentView(R.layout.activity_t_a_home);
 
         sessionManager = new SessionManager(getApplicationContext());
+        groups = findViewById(R.id.Groups);
+        selectCourse = findViewById(R.id.Courses);
 
+
+        UserAPI userAPI = APIClient.getClient().create(UserAPI.class);
+        Call<ArrayList<String>> call = userAPI.getTaughtCourses(sessionManager.getId());
+        call.enqueue(new Callback<ArrayList<String>>() {
+            @Override
+            public void onResponse(Call<ArrayList<String>> call, Response<ArrayList<String>> response) {
+                if (response.code() != 200) {
+                    Toast.makeText(getApplicationContext(), "an error occurred", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    for(int i=0; i<response.body().size(); i++){
+                        String balf = response.body().get(i);
+                        givenCourses.add(balf);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<String>> call, Throwable t) {
+
+            }
+
+        });
+
+        givenCourses.add("");
+
+        // use default spinner item to show options in spinner
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, givenCourses);
+        selectCourse.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectCourse.setOnItemSelectedListener(this);
+
+
+
+
+        //TA name and id shown-------------------------------------------------------------------------------------------------------------------------
         TA_name = findViewById(R.id.TAname);
         TA_id = findViewById(R.id.TAid);
         recordAttendance = findViewById(R.id.RecordAttendance);
@@ -44,15 +84,15 @@ public class TA_home extends AppCompatActivity {
         TA_name.setText(sessionManager.getName());
         TA_id.setText(sessionManager.getId().toString());
 
+        //Record attendance button------------------------------------------------------------------------------------------------------------------------
         recordAttendance.setOnClickListener(new View.OnClickListener(){
-            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 UserAPI userAPI = APIClient.getClient().create(UserAPI.class);
 
                 java.util.Date date=new java.util.Date();
 
-                Call<String> call = userAPI.taStartAttendance(new SimpleDateFormat("dd-MM-yyyy").format(date),"CS_DS_1","CS467",sessionManager.getId());
+                Call<String> call = userAPI.taStartAttendance(new SimpleDateFormat("dd-MM-yyyy").format(date),groups.getText().toString(),selectCourse.getSelectedItem().toString(),sessionManager.getId());
                 call.enqueue(new Callback<String>() {
                     @Override
                     public void onResponse(Call<String> call, Response<String> response) {
@@ -69,4 +109,14 @@ public class TA_home extends AppCompatActivity {
     }
         });
 }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "selected: " + text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    }
 }
--- app/src/main/java/com/example/attendance/UserAPI.java
+++ app/src/main/java/com/example/attendance/UserAPI.java
@@ -1,6 +1,8 @@
 package com.example.attendance;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -18,4 +20,8 @@ public interface UserAPI {
     @POST("ta/postattendance")
     Call<String> taStartAttendance(@Query("date") String date,
                           @Query("userGroup") String userGroup, @Query("courseId") String courseId, @Query("userId") Integer userId);
+
+    @GET("ta/getTaughtCourses")
+    Call<ArrayList<String>> getTaughtCourses(@Query("userId") Integer userId);
+
 }

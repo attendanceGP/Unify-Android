ed5c1b20ec8afe38e39d28de1f5a37d867a17102
alialfie <alisamy27@gmail.com>
2021-05-11 19:40:52 +0200

    fixed deadlines ui to look the same on all sizes

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.attendance.Deadline.DeadlineStudentActivity;
+import com.example.attendance.Deadline.DeadlineTAActivity;
 
 public class MainActivity extends AppCompatActivity {
     SessionManager sessionManager;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(MainActivity.this, DeadlineStudentActivity.class));
 
         // if logged in go to home page
         sessionManager = new SessionManager(getApplicationContext());

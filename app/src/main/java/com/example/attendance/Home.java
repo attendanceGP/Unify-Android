05af2ad681f493a47cda0cfdc9c4f82a260ee8c9
youssef-Ehab <you11098@gmail.com>
2021-05-31 15:51:35 +0200

    added the ta location permission

--- app/src/main/java/com/example/attendance/Home.java
+++ app/src/main/java/com/example/attendance/Home.java
@@ -64,13 +64,6 @@ public class Home extends AppCompatActivity implements LocationListener {
         button = (Button) findViewById(R.id.attend_butt);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, CODE);
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
